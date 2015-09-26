970e556c43d6c5d836c80899623e5d0dadb889df
Chuiwen Ma <machuiwen@gmail.com>
2015-09-26 00:23:54 -0700

    2015-09-25

---
+++ Easy/PalindromeNumber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false; // Only positive number can be palindrome
+        int div = 1;
+        while (x / div >= 10) {
+            div *= 10;
+        }
+        while (x > 0) {
+            if (x % 10 != x / div) return false;
+            x %= div;
+            x /= 10;
+            div /= 100;
+        }
+        return true;
+    }
+};
